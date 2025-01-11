22a265ea104db6b4529b7e7a1752c9dc273659fe
Amir Aijaz <77732195+amiraijaz@users.noreply.github.com>
2025-01-11 19:48:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import os
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Load model and tokenizer
 model_name = "google/gemma-2-2b-it"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, torch_dtype=torch.bfloat16, device_map="auto"
+)
 
 def get_response(input_data):
     input_data = json.loads(input_data)
@@ -17,11 +20,11 @@ def get_response(input_data):
     output = tokenizer.decode(response[0], skip_special_tokens=True)
     return {"output": output}
 
-
 demo = gr.Interface(
-    fn = get_response,
-    inputs = "json",
-    outputs = "json"
+    fn=get_response,
+    inputs="json",
+    outputs="json"
 )
 
-demo.queue().launch(share=True, debug=True, show_error=True)
+if __name__ == "__main__":
+    demo.queue().launch(share=True, debug=True, show_error=True)
